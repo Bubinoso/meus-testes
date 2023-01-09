721df5dada696c6001a73ebbb308fb8b875d66a5
Ramon Camolez <102914608+Bubinoso@users.noreply.github.com>
2023-01-09 15:07:26 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,5 @@ for(int i=0;i<4;i++)
     Console.WriteLine("Digite a nota: ");
     double n = double.Parse(Console.ReadLine());
     numero = n + numero;
-
 }
 Console.WriteLine($"A média é: {numero/4}");
